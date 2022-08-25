4250b3876aec778841a43cd5139b15830963b9fa
shunmanbe <sn.engft@gmail.com>
2022-08-25 16:24:24 +0900

    ビューの継承, トップページ・詳細ページの作成

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>['auth']], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+});
