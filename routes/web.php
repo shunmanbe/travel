a40c34aac0a90988c5fb9f1adbe2c085c5efeae6
shunmanbe <sn.engft@gmail.com>
2022-08-27 07:40:07 +0900

    ItineraryController修正

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Auth::routes();
 
 Route::group(['middleware'=>['auth']], function(){
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('/', 'MakeController@index');
-    Route::get('/itinerary', 'MakeController@index');
+    Route::get('/', 'ItineraryController@index');
+    Route::get('/itinerary', 'ItineraryController@index');
     
     
     
