0e4599de350f2a3bd79fe91d011d62bd31f965bc
shunmanbe <sn.engft@gmail.com>
2022-09-08 10:00:41 +0900

    top→new_entry_areaへのルーティング

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -9,5 +9,8 @@ use App\Area;
 
 class AreaController extends Controller
 {
-    //
+    public function select(Area $area)
+    {
+        return view('itineraries/new_entry_area')->with(['areas' => $area->get()]);
+    }
 }
