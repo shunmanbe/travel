8ed847ee5e300b08a28251f673e0f7cf0f0e3e16
shunmanbe <sn.engft@gmail.com>
2022-09-14 15:32:57 +0900

    不必要なモデルの削除

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,12 @@ Auth::routes();
 Route::group(['middleware'=>['auth']], function(){
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/', 'ItineraryController@index');
-    Route::get('/itineraries', 'ItineraryController@index');
     Route::get('/itineraries/new_entry/date', 'DetailController@date_select');//日程選択画面へ
     Route::post('/itineraries/new_entry/date_store', 'DetailController@date_store');//日程を保存
-    Route::get('/itineraries/new_entry/{detail}/area', 'DetailController@area_select');//出発エリア選択画面へ
+    Route::get('/itineraries/new_entry/{detail}/area', 'DetailController@show');
+    
+    
+    //Route::get('/itineraries/new_entry/{detail}/area', 'DetailController@area_select');//出発エリア選択画面へ
     Route::post('/itineraries/new_entry/{detail}/area_store', 'DetailController@area_store');//出発エリアを保存
     Route::get('/itineraries/new_entry/{detail}/prefecture', 'DetailController@prefecture_select');//出発都道府県選択画面へ
     Route::post('/itineraries/new_entry/{detail}/prefecture_store', 'DetailController@prefecture_store');//出発都道府県を保存
@@ -32,6 +34,7 @@ Route::group(['middleware'=>['auth']], function(){
     //Route::post('itineraries/new_entry/{detail}/railroute', 'RailrouteController@index');
     
     Route::get('/itineraries/{itinerary}', 'DetailController@show');
+   
    
 
     
