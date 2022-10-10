1fcb3a8e3cc967157648940a3d699302249e7f21
shunmanbe <sn.engft@gmail.com>
2022-10-10 20:52:30 +0900

    デプロイ作業途中

--- resources/views/itineraries/search_departure_place.blade.php
+++ resources/views/itineraries/search_departure_place.blade.php
@@ -8,20 +8,21 @@
 
         <!-- Fonts -->
         <link href="https:fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{ asset('/css/search_departure.css')  }}" >
       
     </head>
     <body>
         @extends('layouts.app')
         @section('content')
         <!--出発地を選択-->
-        <h1>出発する地域を検索</h1> 
-        <form action="/itineraries/{{$detail->id}}/departure_place_map" method="POST">
-            @csrf
-            <input type="search_name" name="search_name" placeholder="例：東京 マック">
-            <input type="submit" value="検索">
-        </form>
-        
+        <div class ="select">
+            <h1>出発する地域を検索</h1> 
+            <form action="/itineraries/{{$detail->id}}/departure_place_map" method="POST">
+                @csrf
+                <input type="search_name" name="search_name" placeholder="例：東京 マック">
+                <input type="submit" value="検索">
+            </form>
+        </div>
         @endsection
        
     </body>
