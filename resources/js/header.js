c5715dec48461cadfe28291a3512ca90197877fb
shunmanbe <sn.engft@gmail.com>
2022-11-07 11:50:38 +0900

    header設定モーダル作成（途中）

---
+++ resources/js/header.js
@@ -0,0 +1,9 @@
+$(function() {
+   $('.open-setting').click(function(){
+      $('.setting-modal-contents').fadeIn();
+   });
+   
+   $('.close-setting').click(function(){
+      $('.setting-modal-contents').fadeOut();
+   });
+});
