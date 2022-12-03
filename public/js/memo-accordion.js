a6fea8593680479f9e954360356cd0e0410dcc7d
shunmanbe <sn.engft@gmail.com>
2022-12-03 16:28:42 +0900

    メモ表示をモーダルからアコーディオンに変更完了

--- public/js/memo-accordion.js
+++ public/js/memo-accordion.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -11007,67 +11007,45 @@ return jQuery;
 
 /***/ }),
 
-/***/ "./resources/js/like.js":
-/*!******************************!*\
-  !*** ./resources/js/like.js ***!
-  \******************************/
+/***/ "./resources/js/memo-accordion.js":
+/*!****************************************!*\
+  !*** ./resources/js/memo-accordion.js ***!
+  \****************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {$(function () {
-  //$()はjQueryのセレクターの書き方。$はjQueryの略。
-  var like = $('.like-toggle'); //like-toggleのついたiタグを取得し代入。
-  var likeItineraryId; //変数を宣言
-  like.on('click', function () {
-    //.onはイベントハンドラー。第一パラメータにイベントの種類、第二パラメータにハンドラとして無名関数。
-    var $this = $(this); //this=イベントの実行した要素＝iタグを代入
-    likeItineraryId = $this.data('itinerary-id'); //iタグに仕込んだdata-itinerary-idの値を取得
+  $('.memo').click(function () {
+    var $memo_body = $(this).find('.memo-body');
+    if ($memo_body.hasClass('open')) {
+      $memo_body.removeClass('open');
+      // slideUpメソッドを用いて、$memo_bodyを隠してください
+      $memo_body.slideUp();
 
-    //ajax処理スタート
-    $.ajax({
-      headers: {
-        //HTTPヘッダ情報をヘッダ名と値のマップで記述
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') //attrはattributeの略。属性。指定された属性の値を取ってくる。
-      },
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('.memo-display').text('open');
+    } else {
+      $memo_body.addClass('open');
+      // slideDownメソッドを用いて、$memo_bodyを表示してください
+      $memo_body.slideDown();
 
-      //↑name属性がcsrf-tokenのmetaタグのcontent属性の値を取得
-      url: '/like',
-      //通信先アドレスで、このURLでweb.phpに接続
-      method: 'POST',
-      //HTTPメソッドの種別を指定
-      data: {
-        //サーバーに送信するデータ
-        'itinerary_id': likeItineraryId //いいねされた投稿のidを送る.HTML内に仕込んだカスタムdata属性の値を取得する。
-      }
-    })
-    //通信成功した時の処理
-    .done(function (data) {
-      // dataにjson($param)が入って帰ってくる
-      //toggleClassメソッドは 指定したクラス名の CSS がある場合は削除を行い、なければ追加するメソッド
-      $this.toggleClass('liked'); //likedクラスのON/OFF切り替え。
-      // $this.next(".like-counter")の場合は、$thisで指定したHTML要素の、次にあるHTML要素を指定して、
-      // その要素に.like-counterクラスが追いてる場合に、.html以降のコードを実行する。
-      $this.next('.like-counter').html(data.itinerary_likes_count);
-      //html(htmlString)はHTMLエンコード済みの文字列を表すオブジェクト。itinerary_likes_countは{itinerary_likes_count:1}というjsonの形で渡ってくる。
-    })
-    //通信失敗した時の処理
-    .fail(function () {
-      console.log('fail');
-    });
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('.memo-display').text('close');
+    }
   });
 });
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
 
-/***/ 1:
-/*!************************************!*\
-  !*** multi ./resources/js/like.js ***!
-  \************************************/
+/***/ 3:
+/*!**********************************************!*\
+  !*** multi ./resources/js/memo-accordion.js ***!
+  \**********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/ec2-user/environment/travel/resources/js/like.js */"./resources/js/like.js");
+module.exports = __webpack_require__(/*! /home/ec2-user/environment/travel/resources/js/memo-accordion.js */"./resources/js/memo-accordion.js");
 
 
 /***/ })
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').
     js('resources/js/like.js', 'public/js').
     js('resources/js/memo-modal.js', 'public/js').
+    js('resources/js/memo-accordion.js', 'public/js').
     js('resources/js/header.js', 'public/js')
     .autoload({
     "jquery": ['$', 'window.jQuery'],
